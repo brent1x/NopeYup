d8207a3383d2218c5eec7b80cc7e423fe8f9f8b8
Brent Dady <brentdady@gmail.com>
2015-07-01 13:28:41 -0700

    Initial Commit

---
+++ NopeYup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NopeYup
+//
+//  Created by Brent Dady on 7/1/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NopeYup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NopeYup
+//
+//  Created by Brent Dady on 7/1/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
